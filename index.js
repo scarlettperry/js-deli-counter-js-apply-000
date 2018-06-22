0ba438990b444036f05fc924a39209dbba8f3ec5
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-22 18:01:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing (katzDeliLine)
      return "There is nobody waiting to be served!";
    }
    else{
-     var name = katzDeliLine[0];
+     var name = katzDeliLine[0]; //var name = katzDeliLine.shift();
      katzDeliLine.shift();
      return "Currently serving "+name+".";
    }
